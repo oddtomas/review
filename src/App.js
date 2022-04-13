f175e70012f922cd6b762e502c96f3aaf9b6b739
oddtomas <oddthomas@csu.fullerton.edu>
2022-04-12 22:56:54 -0700

    wrap up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-// import React, { Component }  from 'react';
-import './App.css';
+// import React, { Component, useState } from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 
-const Person = (props) => {  
+const Person = (props) => {
   return (
     <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: Doe</h2>  
-    <h2>Age: 30</h2>  
+      <h1>Name: {props.name}</h1>
+      <h2>Last Name: {props.lastname}</h2>
+      <h2>Age: {props.age}</h2>
     </>
   );
-}
+};
 
 const App = () => {
-
-  const name = 'Clementine';
+  const [counter, setCounter] = useState(0);
+  const name = "Clementine";
   const isNameShowing = true;
+
+  // loads with the first render
+  useEffect(() => {
+    setCounter(100);
+  }, []); // [] means that it will only run once
+
+  useEffect(() => {
+    alert("Counter changed to " + counter);
+  }, [counter]); // [counter] means that it will only run if counter changes
 
   return (
     <div className="App">
-<h1>Hello {isNameShowing ? name: 'Someone'}!</h1>
-{/* is there a name? is yes display 'test' if not 'there is no name' */}
-{name ? (<>test</>): (<>there is no name</>)}
-{/* call person component */}
-<Person name={'Elliot'} />
-{/* pass props with name='Jenny' */}
-<Person name={'Jenny'} />
+      <h1>Hello {isNameShowing ? name : "Someone"}!</h1>
+      {/* is there a name? is yes display 'test' if not 'there is no name' */}
+      {name ? <>test</> : <>there is no name</>}
+      {/* call person component */}
+      <Person name={"Elliot"} lastname={"Nguyen"} age={1} />
+      {/* pass props with name='Jenny' */}
+      <Person name={"Clementine"} lastname={"Nguyen"} age={4} />
+
+      {/* use state */}
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter - 1)}>-</button>
     </div>
   );
-}
+};
 export default App;
